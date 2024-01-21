e555d97cf113fc4b2acda871dbc23c2c26adcc40
MilaisTaso <livreblanche819@gmail.com>
2024-01-21 22:20:47 +0900

    create project and base settiongs

---
+++ .eslintrc.cjs
@@ -0,0 +1,60 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2021: true },
+  extends: ['airbnb', 'airbnb/hooks', 'airbnb-typescript', 'prettier'],
+  ignorePatterns: [
+    'dist',
+    '.eslintrc.cjs',
+    'prettier.config.cjs',
+    '.stylelintrc.cjs',
+    'vite.config.ts',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    // eslint-config-airbnb（eslint)
+    'spaced-comment': 'off',
+    'no-console': 'off',
+    'no-alert': 'off',
+    'arrow-body-style': 'off',
+    // eslint-config-airbnb（eslint-plugin-import)
+    'import/prefer-default-export': 'off',
+    // eslint-config-airbnb（eslint-plugin-react）
+    'react/require-default-props': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/function-component-definition': [
+      2,
+      { namedComponents: 'arrow-function' },
+    ],
+    // eslint-config-airbnb（eslint-plugin-react-hook）
+    'react-hooks/exhaustive-deps': 'warn',
+    // eslint-config-airbnb-typescript（eslint-typescript）
+    '@typescript-eslint/naming-convention': 'off',
+    '@typescript-eslint/no-unused-expressions': [
+      'error',
+      { allowTernary: true },
+    ],
+    '@typescript-eslint/no-unused-vars': 'off',
+    // eslint-plugin-react-refresh
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    // airbnb import/extensions
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        "": "never",
+        "js": "never",
+        "jsx": "never",
+        "ts": "never",
+        "tsx": "never"
+      },
+    ],
+  },
+};
