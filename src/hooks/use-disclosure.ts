040fcadc260a480de48d416f61db512f339e59cb
tatsuki.onoda <tatsuki.onoda@arsaga.jp>
2024-02-28 16:55:57 +0900

    add dialog components

---
+++ src/hooks/use-disclosure.ts
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+// モーダルウインド制御表のhooks
+export const useDisclosure = (initial = false) => {
+  const [isOpen, setIsOpen] = React.useState(initial);
+
+  const open = React.useCallback(() => setIsOpen(true), []);
+  const close = React.useCallback(() => setIsOpen(false), []);
+  const toggle = React.useCallback(() => setIsOpen((state) => !state), []);
+
+  return { isOpen, open, close, toggle };
+};
